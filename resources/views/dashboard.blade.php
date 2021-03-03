107313025e808119c2bbdf54bb974eacadbe6c95
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-03 14:48:04 +0530

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,14 +13,18 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+	    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
         <!-- Styles -->
-        <style>.container {margin-top: 40px;}.btn-primary {width: 100%;}
+        <style>
+        	.container {margin-top: 40px;}.btn-primary {width: 100%;}
+        	.gj-datepicker-md { width: 100%; }
+        	.gj-datepicker-md [role=right-icon] {
+        		top: 20%;
+        	}
         </style>
     </head>
     <body>
@@ -54,7 +58,7 @@
             <div class='col-md-6'>
                <div class="form-group">
                   <label class="control-label">Pick Up</label>
-                  <div class='input-group date' id='datetimepicker1'>
+                  <div class='input-group date'>
                      <input type='text' name="pick_up" class="form-control" placeholder="Pick Up Date" id="startdate" required/>
                      <span class="input-group-addon">
                      <span class="glyphicon glyphicon-calendar"></span>
@@ -65,7 +69,7 @@
             <div class='col-md-6'>
                <div class="form-group">
                		<label class="control-label">Expected Drop</label>
-                  	<div class='input-group date' id='datetimepicker2'>
+                  	<div class='input-group date' >
                      <input type='text' name="expected_drop" class="form-control" placeholder="Expected Drop Date" id="enddate" required/>
                      <span class="input-group-addon">
                      <span class="glyphicon glyphicon-calendar"></span>
@@ -112,21 +116,24 @@
 </div>
 <script type="text/javascript">
 	$(function () {
-		var today = "{{ $today }}";
-		$("#datetimepicker1").datetimepicker({
-			format : 'DD-MMM-YYYY HH:mm',
-			minDate:new Date()
-	    }).on('dp.change', function (event) {
-	        var minDate = new Date(event.date.valueOf());
-	        $('#datetimepicker2').data("DateTimePicker").minDate(minDate);
-    	});
-    
-        $("#datetimepicker2").datetimepicker({
-			format : 'DD-MMM-YYYY HH:mm',
-	    }).on('dp.change', function (event) {
-	        var minDate1 = new Date(event.date.valueOf());
-	        $('#datetimepicker1').data("DateTimePicker").maxDate(minDate1);
-    	});
+		var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+        $('#startdate').datepicker({
+            uiLibrary: 'bootstrap4',
+            iconsLibrary: 'fontawesome',
+            minDate: today,
+            maxDate: function () {
+                return $('#enddate').val();
+            }
+        });
+        $('#enddate').datepicker({
+            uiLibrary: 'bootstrap4',
+            iconsLibrary: 'fontawesome',
+            minDate: function () {
+                return $('#startdate').val();
+            }
+        });
+
+		
 
     	$('#station-vehicle').on('change', function() {
     		var fromDate = new Date($("#startdate").val());
