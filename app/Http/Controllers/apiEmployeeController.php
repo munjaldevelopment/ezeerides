4e653fda0e2d1bee670f92dc30bcf9cc10f4fbdb
Ashok Kumar Sharma <ashok.sharma@microprixs.in>
2021-07-01 15:50:43 +0530

    Update apiEmployeeController.php

--- app/Http/Controllers/apiEmployeeController.php
+++ app/Http/Controllers/apiEmployeeController.php
@@ -1779,7 +1779,7 @@ class apiEmployeeController extends Controller
                     }
                     $current_time = date('H:i:s');
 
-                    $booked_vehicleList = DB::table('vehicle_registers')->select('id','vehicle_model_id','booking_no','user_id','customer_id', 'customer_name','pick_up','pick_up_time','expected_drop','expected_drop_time','station','vehicle','additional_amount', 'receive_amount','is_amount_receive','receive_date','status')->where('user_id',$employee_id)->where('booking_status','1')->where('status','In')->where('vehicle','=', '')->where('due_penalty','no')->wheredate('expected_drop', '<=', $filterdate)->where('expected_drop_time', '<', $current_time);
+                    $booked_vehicleList = DB::table('vehicle_registers')->select('id','vehicle_model_id','booking_no','user_id','customer_id', 'customer_name','pick_up','pick_up_time','expected_drop','expected_drop_time','station','vehicle','additional_amount', 'receive_amount','is_amount_receive','receive_date','status')->where('user_id',$employee_id)->where('booking_status','1')->where('due_penalty','no')->wheredate('expected_drop', '<=', $filterdate)->where('expected_drop_time', '<', $current_time);
 
                     if($center){
                         $booked_vehicleList = $booked_vehicleList->where('station',$station_name);    
@@ -1804,13 +1804,13 @@ class apiEmployeeController extends Controller
                             $vehicleModel = DB::table('vehicle_models')->where('id', $model_id)->pluck('model')[0];
 
                             if($vehicle_status == 'In'){
-                                //$vstatus = 'Prepare To Delivery';
-                                $vstatus = '';
+                                $vstatus = 'Prepare To Delivery';
+                                //$vstatus = '';
                             }
 
                             if($vehicle_status == 'Out'){
-                                //$vstatus = 'Customer Return at Center';
-                                $vstatus = '';
+                                $vstatus = 'Customer Return at Center';
+                                //$vstatus = '';
                             }
 
                             if($vlist->receive_date != '' && $vlist->is_amount_receive === 1){
