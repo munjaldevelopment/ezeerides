a350a6f3b11c3bf99bbcac26b10d475d8c2e04b8
Ashok Kumar Sharma <ashok.sharma@microprixs.in>
2021-07-13 15:31:58 +0530

    Update apiController.php

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -1475,9 +1475,9 @@ class apiController extends Controller
                        $api = new Api(env('RAZOR_KEY'), env('RAZOR_SECRET'));
 
                        $payment = $api->payment->fetch($razorpay_payment_id);
-                        echo '<pre>';
+                       /* echo '<pre>';
                         print_r($payment);
-                        exit;   
+                        exit;*/   
                        $status = $payment['status'];
                         if($status == 'captured'){
                           //Transaction Successful
