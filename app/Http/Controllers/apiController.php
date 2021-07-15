915c4513a4fcaaac1da513b006bc9a9b99c0b4ff
Ashok Kumar Sharma <ashok.sharma@microprixs.in>
2021-07-15 13:40:24 +0530

    api changes

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -1646,7 +1646,8 @@ class apiController extends Controller
         }
         catch(\Exception $e) {
             $status_code = '0';
-            $message = $e->getMessage();//$e->getTraceAsString(); getMessage //
+            //$message = $e->getMessage();//$e->getTraceAsString(); getMessage //
+            $message = $e->getTraceAsString();
     
             $json = array('status_code' => $status_code, 'message' => $message);
         }
